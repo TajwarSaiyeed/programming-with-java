b2eb37b7d40ae7bcefed5efb3d30e03948216d78
tajwarsaiyeed <rjabid36@gmail.com>
2025-02-08 19:04:39 +0600

    Add A_Say_Hello_With_Java class to greet user input

---
+++ 08-02-2025/A_Say_Hello_With_Java.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class A_Say_Hello_With_Java {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.print("Hello, " + s);
+    }
+}
